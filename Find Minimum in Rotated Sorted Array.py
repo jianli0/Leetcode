d40f84e2e0420b058334a16fa17eb4b9c241d42d
xljsskk <jli@ccs.neu.edu>
2015-02-24 16:30:49 -0500

    Find Minimum in Rotated
    problemed sovled. Remember to return layer by layer

--- Find Minimum in Rotated Sorted Array.py
+++ Find Minimum in Rotated Sorted Array.py
@@ -3,25 +3,29 @@ class Solution:
     # @return an integer
     def findMin(self, num):
         print "num is %r"%(num) ##########
-        if num:
-            if len(num) == 1:
-                return num[0]
+        if len(num) == 1:
+            print "num[0] is %d"%(num[0])
+            return num[0]
+        else:
+            print "start end median"#######
+            start = 0
+            end = len(num) - 1
+            median = end / 2
+            print "%r %r %r"%(start,end,median)#######
+            if num[median] < num[median-1]:
+                return num[median]
+            elif num[median] > num[end]:
+                a = num[median+1:end+1]
+                return self.findMin(a)
             else:
-                start = 0
-                end = len(num) - 1
-                median = end / 2
-                print "start end median"#######
-                print "%r %r %r"%(start,end,median)#######
-                if num[median] < num[median-1]:
-                    return num[median]
-                elif num[median] > num[end]:
-                    self.findMin(num[median+1:end+1])
-                else:
-                    self.findMin(num[start:median])
+                a = num[start:median]
+                return self.findMin(a)
 
-print Solution().findMin([0, 1, 2, 3, 4, 5, 6, 7])
-print Solution().findMin([2, 3, 4, 5, 6, 7, 0, 1])
-print Solution().findMin([3, 4, 5, 6, 7, 0, 1, 2])
-print Solution().findMin([5, 6, 7, 0, 1, 2, 3, 4])
+print Solution().findMin([2])
+print Solution().findMin([2,1])
+#print Solution().findMin([0, 1, 2, 3, 4, 5, 6, 7])
+#print Solution().findMin([2, 3, 4, 5, 6, 7, 0, 1])
+#print Solution().findMin([3, 4, 5, 6, 7, 0, 1, 2])
+#print Solution().findMin([5, 6, 7, 0, 1, 2, 3, 4])
 
 
