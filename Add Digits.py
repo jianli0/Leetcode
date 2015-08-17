67ba2f449624f0e6c8df2ee63a91d5c1ab9ee62c
xljsskk <jli@ccs.neu.edu>
2015-08-17 10:17:04 -0400

    Add Digits Done
    Recursively and non-Recursively

--- Add Digits.py
+++ Add Digits.py
@@ -1,26 +1,27 @@
 # Solution 1
-# class Solution:
-    # # @param {integer} num
-    # # @return {integer}
-    # def addDigits(self, num):
-        # if num < 10:
-            # return num
-        # res = num % 10
-        # while True:
-            # res += num % 10
-            # is_digit = num / 10
-        # return addDigits(res)
-
-# Solution 2
 class Solution:
     # @param {integer} num
     # @return {integer}
     def addDigits(self, num):
-        if num % 9 :
-            return num % 9
-        elif num:
-            return 9
-        else:
-            return 0
+        if num < 10:
+            return num
+        res = num % 10
+        while num/10 > 0:
+            num = num/10
+            res += num % 10
+
+        return self.addDigits(res)
+
+# Solution 2
+# class Solution:
+    # # @param {integer} num
+    # # @return {integer}
+    # def addDigits(self, num):
+        # if num % 9 :
+            # return num % 9
+        # elif num:
+            # return 9
+        # else:
+            # return 0
 
 print Solution().addDigits(38)
